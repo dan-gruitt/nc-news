54d3b68e9c002e9b9c43b31f49757a37c381f2e0
Daniel Gruitt <danielgruitt@Daniels-MBP.wifi.quayside.net.generatorhub.co.uk>
2024-02-07 12:09:23 +0000

    this commit completes the core functionality of task 6, adds comments and comment likes

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,9 +1,28 @@
-const CommentList = () => {
+import { useEffect, useState } from "react";
+import CommentCard from "./CommentCard"
+import { getCommentsByArticleID } from "../utils/getCommentsByArticleID";
+
+const CommentList = ({ article_id }) => {
+
+    const [comments, setComments] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(() => {
+        getCommentsByArticleID(article_id).then(({ comments }) => {
+            setComments(comments)
+            setIsLoading(false)
+        })
+    }, [article_id])
+
+
     return (
-        <h2>
-            Im the comment list
-        </h2>
-      );
+        <section className="comment-box">
+            < CommentCard comments={comments} />
+        </section>
+
+
+    );
 }
- 
+
 export default CommentList;
